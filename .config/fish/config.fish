a65bad3f05e70fa954eaf9efa1498601538785d3
Alexandre Louvet <alexandre.louvet8@gmail.com>
2021-01-28 14:49:46 +0900

    starship + conda config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,6 @@
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-#eval /home/alexandre/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+eval /home/alexandre/miniconda3/bin/conda "shell.fish" "hook" $argv | source
 # <<< conda initialize <<<
 
 starship init fish | source
